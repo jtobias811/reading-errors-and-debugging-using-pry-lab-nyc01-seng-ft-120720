ac3502b647dd7c8428ad7569241e6d9778817004
Jacob Tobias <jtobias811@gmail.com>
2020-10-29 19:23:15 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,12 +1,10 @@
 # don't forget to add: require 'pry'
-string = "surprise"
+
 def snake_it_up(string)
   if string[0] == "s"
-    10.times do "s" + string
-      binding.pry
-    end
+    10 * "s" + string
+    partition { |e|  }
   else
     string
-    binding.pry
   end
 end
